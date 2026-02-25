224dddf6447d25eac254ca4e7fbfd8a8e601a352
suedsonjunior <suedsonfernando30@gmail.com>
2026-02-25 00:25:18 +0000

    aula 3

---
+++ aula2/pesquisaidade.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PesquisaIdade {
+    public static scanner input = new Scanner (System.in);
+    public static void main(String[] args) {
+        int idade, somaIdades = 0, qtdPessoas = 0;
+       
+        System.out.println("Digite a sua idade:");
+        idade = input.nextInt();
+        while (idade >= 0)  {
+            qtdPessoas += 1;
+            somaIdades += idade;
+
+            System.out.println("Digite a sua idade (< 0 para sair): ");
+            idade = input.nextInt();
+        }
+        if (qtdPessoas != 0){
+            double media = (double) somaIdades / qtdPessoas;
+            System.out.println(qtdPessoas + " pessoas participaram da pesquisa.");
+            System.out.println("A idade média dos participantes foi de " + media + " anos.");
+        } else {
+            System.out.println("Nenhuma pessoa participou da pesquisa!");
+            
+        }
+    }
+}
+
+      
